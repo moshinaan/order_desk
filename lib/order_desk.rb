0c608cd384cbedc10fc5064541bb02fa8fb889b1
Anastasiia Moshina <moshinaan@gmail.com>
2026-02-05 13:58:46 +0400

    Add update order request support

--- lib/order_desk.rb
+++ lib/order_desk.rb
@@ -6,6 +6,7 @@ require_relative 'order_desk/requests/base'
 require_relative 'order_desk/requests/test_connection'
 require_relative 'order_desk/requests/get_order'
 require_relative 'order_desk/requests/get_orders'
+require_relative 'order_desk/requests/update_order'
 
 module OrderDesk
 end
